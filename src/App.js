7d3c6a2d0c74549e45344dd9db65e628434fac9a
Corina Morar <adnana108@yahoo.com>
2024-12-10 15:42:54 +0200

    basic nav bar implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './Home/Home';
 import ResultPage from './ResultPage/ResultPage';
+import NavBar from "./Components/NavBar";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import "./NavBar.scss"
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>HomePage</p>
+            <p>Blog</p>
+            <p>your favorite beaches</p>
+            <p>signin</p>
+        </div>
+    )
+}
+
+export default NavBar;
